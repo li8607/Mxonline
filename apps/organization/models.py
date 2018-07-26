80e3736b446ef384754d262ecd6e88d8b3938679
li8607 <675669728@qq.com>
2018-07-26 15:33:11 +0800

    添加机构分类功能

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -1,3 +1,44 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+class CityDict(models.Model):
+    name = models.CharField(max_length=20, verbose_name="城市")
+    desc = models.CharField(max_length=200, verbose_name="描述")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "城市"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class CourseOrg(models.Model):
+    ORG_CHOICES = (
+        ("pxjg", u"培训机构"),
+        ("gx", u"高校"),
+        ("gr", u"个人"),
+    )
+    name = models.CharField(max_length=50, verbose_name="机构名称")
+    desc = models.TextField(verbose_name="机构描述")
+    category = models.CharField(max_length=20, choices=ORG_CHOICES, verbose_name="机构类别", default="pxjg")
+    tag = models.CharField(max_length=10, default="国内名校", verbose_name="机构标签")
+    click_nums = models.IntegerField(default=0, verbose_name="点击数")
+    fav_nums = models.IntegerField(default=0, verbose_name="收藏数")
+    image = models.ImageField(upload_to="org/%Y/%m", verbose_name="Logo", max_length=100)
+    address = models.CharField(max_length=150, verbose_name="机构地址")
+    city = models.ForeignKey(CityDict, on_delete=models.CASCADE, verbose_name="所在城市")
+    students = models.IntegerField(default=0, verbose_name="学习人数")
+    course_nums = models.IntegerField(default=0, verbose_name="课程数")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "课程机构"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "课程机构{}".format(self.name)
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.views.generic.base import View
 
+from .models import CityDict, CourseOrg
+
 
 class OrgView(View):
+
     def get(self, request):
-        return render(request, 'org-list.html')
+        all_city = CityDict.objects.all()
+        all_orgs = CourseOrg.objects.all()
+        org_nums = all_orgs.count()
+        city_id = request.GET.get('city', '')
+
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        category = request.GET.get('ct', '')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        return render(request, 'org-list.html', {
+            "all_orgs": all_orgs,
+            "all_city": all_city,
+            "org_nums": org_nums,
+            "city_id": city_id,
+            "category": category,
+        })
