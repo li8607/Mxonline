8fc9050000d964ec252e900ca7bc08a63ff5b6bc
li8607 <675669728@qq.com>
2018-08-01 11:00:24 +0800

    添加机构教师&机构介绍。

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 
-from organization.views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView
+from organization.views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView
 
 app_name = 'org'
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('list/', OrgView.as_view(), name='list'),
     path('add_ask/', AddUserAskView.as_view(), name='add_ask'),
     re_path('home/(?P<org_id>\d+)/', OrgHomeView.as_view(), name='org_home'),
-    re_path('course/(?P<org_id>\d+)/', OrgCourseView.as_view(), name='org_course')
+    re_path('course/(?P<org_id>\d+)/', OrgCourseView.as_view(), name='org_course'),
+    re_path('desc/(?P<org_id>\d+)/', OrgDescView.as_view(), name='org_desc'),
+    re_path('teacher/(?P<org_id>\d+)/', OrgTeacherView.as_view(), name='org_teacher')
 ]
