5a6d19a80b7ad2ab974a6b274a9a8b8955731a94
li8607 <675669728@qq.com>
2018-08-01 17:32:34 +0800

    优化公开课。

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-
-
 # Create your views here.
 from django.views.generic.base import View
+
+from courses.models import Course
 
 
 class CourseListView(View):
 
     def get(self, request):
-        return render(request, 'course-list.html')
+        all_course = Course.objects.all()
+        sort = request.GET.get("sort", "")
+        hot_course = all_course.order_by("-click_nums")[:5]
+        if sort and sort == "hot":
+            all_course = all_course.order_by("-click_nums")
+        elif sort and sort == "students":
+            all_course = all_course.order_by("-students")
+        else:
+            all_course = all_course.order_by("-add_time")
+
+        return render(request, 'course-list.html', {
+            "all_course": all_course,
+            "sort": sort,
+            "hot_course": hot_course,
+        })
