13a29faa59c53ce3b520f0f50fda56bd0547f403
li8607 <maverick_8090@163.com>
2018-08-09 05:51:08 +0800

    部署设置

--- Mxonline/settings.py
+++ Mxonline/settings.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 SECRET_KEY = 'tbf6vgb*0!yiq1k1jrw%_6=tc@811b60(a8xrhlx=-9_cs)3f*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', 'mxonline.bencheng.xyz']
 
